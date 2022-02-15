37c58b8ed897f67d0936f59288ec958397e98252
Akshat Surolia <ace@Akshats-MacBook-Air.local>
2022-02-15 14:02:39 +0530

    removing keys

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,7 @@
 var s = document.createElement('script'); 
 s.type = 'text/javascript'; s.async = true; 
 s.src = 'js/bot.js'; 
-document.getElementById('botcopy-embedder-d7lcfheammjct').appendChild(s);
+document.getElementById('botcopy-embedder-xxxx').appendChild(s);
 
 navigator.mediaDevices.getUserMedia({ audio: true })
       .then(function(stream) {
